7fe39e4c7b86b09da1abc5d9619c5d2c3d82d91f
jleon <jleon@optimeconsulting.com.co>
2018-09-26 09:05:07 -0500

    Primeros Controladores
    Cupon controller, Default, OfertaRepoasitory and config.yml

---
+++ src/AppBundle/Controller/CuponController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JLEON
+ * Date: 9/25/2018
+ * Time: 9:15 AM
+ */
+
+namespace AppBundle\Controller;
+
+
+class CuponController
+{
+
+}
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,23 +2,17 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Oferta;
+use Doctrine\ORM\Mapping as Embedded;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
-{
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ));
-    }
+/**
+ *
+ */
+class DefaultController extends Controller{
 
     /**
      * @Route(
@@ -31,5 +25,76 @@ class DefaultController extends Controller
     public function paginaAction($nombrePagina)
     {
         return $this->render('sitio/'.$nombrePagina.'.html.twig');
+    }
+
+    /**
+     * @Route("/{ciudad}", defaults={ "ciudad" = "%app.ciudad_por_defecto%" }, name="portada")
+     * @Route("/")
+     */
+    public function portadaAction($ciudad){
+
+        if ($ciudad === null){
+            return $this->redirectToRoute('portada', array(
+               'ciudad' => $this->getParameter('app.ciudad_por_defecto')
+            ));
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $oferta = $em->getRepository('AppBundle:Oferta')->findOneBy(array(
+            'ciudad' => $this->getParameter('app.ciudad_por_defecto'),
+            'fechaPublicacion' => new \DateTime('today')
+        ));
+
+        if (!$oferta){
+            throw $this->createNotFoundException(
+                'No se ha encontrado la oferta del dia en la ciudad seleccionada'
+            );
+        }
+
+        return $this->render('portada.html.twig', array(
+           'oferta' => $oferta
+        ));
+
+        //        $emOferta = $this->getDoctrine()->getRepository('AppBundle:Oferta');
+//        $emCiudad = $this->getDoctrine()->getRepository('AppBundle:Ciudad');
+//
+//        $oferta = $emOferta->find(1);
+//
+//        $precio = $oferta->getPrecio();
+//
+//        $allOfertas = $emOferta-<findAll();
+//
+//        // Encontrar todas las ofertas revisadas
+//        $ofertasRevisadas = $emOferta->findBy(
+//            array(
+//                'revisada' => true
+//            )
+//        );
+//
+//        // Encontrar la ciudad de Vitoria-Gasteiz
+//        $ciudad = $emCiudad->findBy(array(
+//            'ciudad' => $ciudad->getId(),
+//            'permiteEmail' => true
+//        ));
+//
+//        $losMasVeteranos = $em->getRepository('AppBundle:Usuario')->findBy(
+//            array( 'permiteEmail' => true ), /*Criterio de Busqueda*/
+//            array( 'nombre' => 'ASC', 'fechaNacimiento' => 'DESC' ), /*Order*/
+//            10, /*# de resultados*/
+//            0 /*Empieza en posicion 0*/
+//        );
+//
+//        $oferta = new Oferta();
+//        $oferta->setNombre('Nombre');
+//        $oferta->setPrecio(10.99);
+//        $oferta->setDescripcion('Descripcion');
+//        $oferta->setCondiciones('Lorem Ipsum');
+//        $oferta->setDescuento(5.5);
+//
+//        $entityManager = $this->getDoctrine()->getManager()M
+//        $entityManager->persist($oferta);
+//        $entityManager->flush();
+
+
     }
 }
