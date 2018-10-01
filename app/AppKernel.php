4cc8a58a1942f09c7bb4ebfc087dbcb9eba37445
jleon <jleon@optimeconsulting.com.co>
2018-10-01 15:38:57 -0500

    Agrega upload data bundle
    Se agrega bundle para probar carga de archivos en excel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,10 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            //  upload data bundle ->
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new Ku\AjaxBundle\KuAjaxBundle(),
+            new Manuel\Bundle\UploadDataBundle\UploadDataBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
